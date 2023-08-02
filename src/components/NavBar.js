d054a815c2a0396f956508b0cee23e761abc3ddd
OmarMWarraich <earthling986@gmail.com>
2023-08-03 02:13:01 +0500

    style: Micro Interactions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {
-  AppBar, Toolbar, Typography, useMediaQuery,
+  AppBar,
+  Toolbar,
+  Typography,
+  useMediaQuery,
 } from '@mui/material';
 import { motion } from 'framer-motion';
 
@@ -9,17 +12,25 @@ const NavBar = () => {
 
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1, textAlign: 'center', color: 'black' }}>
-          <motion.h1
+      <Toolbar sx={{
+        display: 'flex', height: '8rem', alignItems: 'center',
+      }}
+      >
+        <Typography
+          variant="h3"
+          sx={{
+            flexGrow: 1, textAlign: 'center', color: 'black', fontWeight: 'bold', fontFamily: 'Roboto', letterSpacing: '0.1rem',
+          }}
+        >
+          <motion.div
             initial={{ scale: 1, opacity: 0, y: 0 }}
             animate={isMobile
               ? { scale: 1.2, y: 120, opacity: 1 }
-              : { scale: 2, y: 120, opacity: 1 }}
+              : { scale: 1.6, y: 120, opacity: 1 }}
             transition={{ duration: 0.5, ease: 'linear', type: 'tween' }}
           >
             Task Manager
-          </motion.h1>
+          </motion.div>
         </Typography>
       </Toolbar>
     </AppBar>
